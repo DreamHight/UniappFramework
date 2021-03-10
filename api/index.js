527f5db2e7f53eea324b2e52b4e5f18530f72d00
kevin chen <kevin@kevindeMacBook-Pro.local>
2021-03-10 23:55:43 +0800

    home

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
+// api封装的目标：
+// 1. 接口api化
+// 2. 请求自动绑定
+// 3. 防止重复提交
+
 import service from "../utils/service"
 import getGoodRegisterCheckDetail from "../views/model1/business1/services/meat"
 
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
+// 格式化日期时间
 const formatNumber = n => {
 	n = n.toString()
 	return n[1] ? n : '0' + n
